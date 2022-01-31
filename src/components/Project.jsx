ec364c0dc59d48b23abe063bbcb4d080c2eaa9f6
Poom Muangthai <poom.mth@hotmail.com>
2022-01-31 08:42:35 +0700

    Change project flex to grid for responsive

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,10 +2,10 @@ import ProjectData from './ProjectData'
 import ProjectItem from './ProjectItem'
 const Project = () => {
    return (
-      <div >
+      <div id="project" >
          <h3 className="font-medium">Project</h3>
       <br />
-         <div className="flex gap-5">
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center">
             {
                ProjectData.map((el, index) => {
                   return <ProjectItem key={index} image={el.image} title={el.title} description={el.description} siteLink={el.siteLink} repoLink={el.repoLink}></ProjectItem>
