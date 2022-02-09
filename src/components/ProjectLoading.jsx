cca9a9570a515cca52077bf5accd6e62dd41184c
Poom Muangthai <poom.mth@hotmail.com>
2022-02-09 08:33:14 +0700

    Fix project loading grid invalid display

--- src/components/ProjectLoading.jsx
+++ src/components/ProjectLoading.jsx
@@ -1,20 +1,18 @@
-import { data } from "autoprefixer"
-
 const ProjectLoading = () => {
 
    const Boxes = () => {
       const boxes = []
       for (let i = 0; i < 4; i++) {
-          boxes.push(<div key={i} className="w-full h-80 bg-slate-200 animate-pulse"></div>)
+         boxes.push(<div key={i} className="w-full h-80 bg-slate-200 animate-pulse"></div>)
       }
       return boxes
    }
 
    return (
-      <div>
+      <div >
          <div className="w-full h-10 bg-slate-200 animate-pulse mb-4"></div>
-         <div className="grid-cols-4 grid gap-10">
-           <Boxes></Boxes>
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center">
+            <Boxes></Boxes>
          </div>
       </div>
    )
